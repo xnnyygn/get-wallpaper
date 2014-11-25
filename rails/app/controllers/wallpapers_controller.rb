9080d4279b79b845c27a3714ee32ec290f331df5
xnnyygn <xnnyygn@gmail.com>
2014-11-25 17:31:05 +0800

    add index

--- rails/app/controllers/wallpapers_controller.rb
+++ rails/app/controllers/wallpapers_controller.rb
@@ -1,5 +1,7 @@
 class WallpapersController < ApplicationController
   def index
+    @image_files = Dir.entries('app/assets/images').select{|n| n.end_with?('.jpg')}
+    logger.debug('IMAGE FILES ' + @image_files.to_s)
   end
 
   def list
